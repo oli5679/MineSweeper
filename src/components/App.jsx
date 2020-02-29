237b6188484b563f9c106a30ea1ec3a4e7d9ebd1
Robin Bernon <robinbernon@hotmail.co.uk>
2020-02-29 01:17:00 +0000

    Using semantic ui for the page design and layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Container, Divider, Grid, Header, Icon } from 'semantic-ui-react'
 
 export default class AppWrapper extends React.Component {
   render() {
     return (
-      <div className='app-container'>
+      <Container>
+    {/* Heads up! We apply there some custom styling, you usually will not need it. */}
+
+    <Header as='h2' icon inverted textAlign='center'>
+      <Icon name='grid layout' />
+      Minesweeper
+      <Header.Subheader>
+        Do you wanna play some minesweeper to remind you of the good old days?
+      </Header.Subheader>
+    </Header>
+    <Divider />
+    <Header as='h2' icon inverted textAlign='center'>
         <Link to={'/'}>Home</Link>
         <Link to={'/play'}>Play</Link>
         {this.props.children}
-      </div>
+      </Header>
+    </Container>
     )
   }
 }

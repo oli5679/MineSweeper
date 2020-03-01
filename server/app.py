2638f5d3d649dc4a5cc19bfd949edd25896bb6ce
Oliver Cairns <o.cairns@marketfinance.com>
2020-03-01 12:34:33 +0000

    added multiple games

--- server/app.py
+++ server/app.py
@@ -3,13 +3,11 @@ from flask_cors import CORS
 import json
 import numpy as np
 import flask
+import uuid
 
 import minesweeper
 
-GAME = minesweeper.Game()
-
-for row in GAME.mine_locations:
-    print(row)
+GAMES = {"TEST": minesweeper.Game()}
 
 app = Flask(__name__, static_folder="../build")
 CORS(app)
@@ -26,9 +24,9 @@ class NumpyEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, obj)
 
 
-def format_game_state(game):
+def find_game_state(game_id):
     """
-    Returns json blob with current state of game
+    Returns json blob with current state of game with specific id
 
     Args:
         game (minesweeper.Game)
@@ -36,7 +34,9 @@ def format_game_state(game):
     Returns:
         response (string): json blog of game state
     """
+    game = GAMES[game_id]
     response = {
+        "game_id": game_id,
         "revealed_state": game.revealed_state,
         "unclicked_non_mine_count": game.unclicked_non_mine_count,
         "has_won": game.has_won,
@@ -50,7 +50,7 @@ def format_game_state(game):
 ##
 
 
-@app.route("/ping", methods=["GET"])
+@app.route("/api/ping", methods=["GET"])
 def health_check():
     """ 
     Health check 
@@ -58,34 +58,35 @@ def health_check():
     return "Response"
 
 
-@app.route("/game_state", methods=["GET"])
-def get_state():
+@app.route("/api/game_state/<string:game_id>", methods=["GET"])
+def get_state(game_id):
     """ 
     Return current state 
     """
-    return format_game_state(GAME)
+    return find_game_state(game_id)
 
 
-@app.route("/click", methods=["POST"])
+@app.route("/api/click", methods=["POST"])
 def make_click():
     """
     Click on cell and return updated game state
     """
     move_inputs = json.loads(flask.request.data)
-    global GAME
-    GAME.click(int(move_inputs["x"]), int(move_inputs["y"]))
-    return format_game_state(GAME)
+    global GAMES
+    GAMES[move_inputs["game_id"]].click(int(move_inputs["x"]), int(move_inputs["y"]))
+    return find_game_state(move_inputs["game_id"])
 
 
-@app.route("/reset", methods=["POST"])
+@app.route("/api/create", methods=["POST"])
 def reset_game():
     """
     Create new game with specified game parameters
     """
     game_inputs = json.loads(flask.request.data)
-    global GAME
-    GAME = minesweeper.Game(**game_inputs)
-    return format_game_state(GAME)
+    game_id = str(uuid.uuid4())
+    global GAMES
+    GAMES[game_id] = minesweeper.Game(**game_inputs)
+    return find_game_state(game_id)
 
 
 ##
--- server/test_app.py
+++ server/test_app.py
@@ -2,6 +2,7 @@ import pytest
 import flask
 import app
 import json
+from unittest.mock import patch
 
 
 @pytest.fixture
@@ -10,23 +11,25 @@ def client():
 
 
 def test_ping(client):
-    response = client.get("/ping")
+    response = client.get("/api/ping")
     assert response.status_code == 200
     assert response.data == b"Response"
 
 
 def test_display_game_state(client):
-    response = client.get("/game_state")
+    response = client.get("/api/game_state/TEST")
     assert response.status_code == 200
 
 
-def test_click(client):
-    response = client.post("/click", data=json.dumps({"x": 0, "y": 1}))
-    assert response.status_code == 200
-
-
-def test_reset(client):
+def test_click(client,):
     response = client.post(
-        "/reset", data=json.dumps({"x_max": 10, "y_max": 10, "num_mines": 20})
+        "/api/click", data=json.dumps({"x": 0, "y": 1, "game_id": "TEST"})
+    )
+    assert response.status_code == 200
+
+
+def test_create(client):
+    response = client.post(
+        "/api/create", data=json.dumps({"x_max": 10, "y_max": 10, "num_mines": 20})
     )
     assert response.status_code == 200
